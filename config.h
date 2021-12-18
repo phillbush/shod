695a8165f77dfaf7061551798870853275cf6560
seninha <lucas@seninha.org>
2021-12-18 15:54:47 -0300

    add config.h

---
+++ config.h
@@ -0,0 +1,48 @@
+struct Config config = {
+	/*
+	 * except for the foreground, colors fields are strings
+	 * containing up to three elements delimited by colon:
+	 * the body color, the color of the light 3D shadow,
+	 * and the color of the dark 3D shadow.
+	 */
+
+	/* general configuration */
+	.snap           = 8,            /* proximity of container edges to perform snap attraction */
+	.font           = "fixed",      /* font for titles in titlebars */
+	.ndesktops      = 10,           /* number of desktops per monitor */
+
+	/* dock configuration */
+	.dockwidth      = 64,           /* width of the dock (or its height, if it is horizontal) */
+	.dockspace      = 64,           /* size of each dockapp (64 for windowmaker dockapps) */
+	.dockgravity    = "E",          /* placement of the dock */
+	.dockcolors     = "#121212:#2E3436:#000000",
+
+	/* notification configuration */
+	.notifgap       = 3,            /* gap, in pixels, between notifications */
+	.notifgravity   = "NE",         /* placement of notifications */
+	.notifcolors    = "#3465A4:#729FCF:#204A87",
+
+	/* prompt configuration */
+	.promptcolors   = "#3465A4:#729FCF:#204A87",
+
+	/* container colors */
+	.titlewidth = 17,
+	.titlecolors = {
+		[FOCUSED]   = "#3465A4:#729FCF:#204A87",
+		[UNFOCUSED] = "#555753:#888A85:#2E3436",
+		[URGENT]    = "#CC0000:#EF2929:#A40000",
+	},
+	.foreground = {
+		[FOCUSED]   = "#FFFFFF",
+		[UNFOCUSED] = "#FFFFFF",
+		[URGENT]    = "#FFFFFF",
+	},
+
+	/* border */
+	.borderwidth = 6,
+	.bordercolors = {
+		[FOCUSED]   = "#3465A4:#729FCF:#204A87",
+		[UNFOCUSED] = "#555753:#888A85:#2E3436",
+		[URGENT]    = "#CC0000:#EF2929:#A40000",
+	},
+};
